f03fadc0b99d4be118e623fca5d3a341f77afe2b
Aman Mavai <amanmavai@gmail.com>
2023-11-19 13:15:45 +0530

    improve BlogPost.astro layout

--- src/components/FormattedDate.tsx
+++ src/components/FormattedDate.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 export default function FormattedDate({date}: Props) {
   return (
-    <time dateTime={date.toISOString()}>
+    <time dateTime={date.toISOString()} className="font-medium text-base">
       {date.toLocaleDateString("en-us", {
         year: "numeric",
         month: "short",
