5c4328087bf8415388bcd7da7cac175f67017c4e
Aman Mavai <amanmavai@gmail.com>
2023-11-21 08:58:40 +0530

    update FormattedDate to have className prop

--- src/components/FormattedDate.tsx
+++ src/components/FormattedDate.tsx
@@ -1,9 +1,12 @@
+import {twMerge} from "tailwind-merge";
+
 interface Props {
   date: Date;
+  className?: string;
 }
-export default function FormattedDate({date}: Props) {
+export default function FormattedDate({date, className}: Props) {
   return (
-    <time dateTime={date.toISOString()} className="text-sm sm:text-base leading-6 text-gray-500 mb-3">
+    <time dateTime={date.toISOString()} className={twMerge("text-sm sm:text-base leading-6 mb-3", className)}>
       {date.toLocaleDateString("en-us", {
         year: "numeric",
         month: "short",
